fd666cc6d488eeb7214e7ffd6d79f2b3aa021b67
dlwltn0350 <dlwltn0350@naver.com>
2022-02-12 23:49:40 +0900

    modify 1157-1

--- baekjoon01/src/level7/n1157.java
+++ baekjoon01/src/level7/n1157.java
@@ -10,7 +10,7 @@ public class n1157 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s= br.readLine();
-		int max=0, tmp=0;
+		int max=0, tmp=0, n=0;
 		
 		int[] arr=new int[26];
 		Arrays.fill(arr, 0);
@@ -21,21 +21,20 @@ public class n1157 {
 				else if((int)s.charAt(i)-j-32==0) arr[j-65]++;
 			}
 		}
+		
 		max=arr[0];
+		
 		for(int i=1;i<26;i++) {
 			if(max<arr[i]) {
 				max=arr[i];
 				tmp=i;
 			}
-			else if(max==arr[i] && max!=0) {
-				tmp=-1;
-				break;
-			}
-		}
+			else if(max==arr[i] && max!=0) n=max;
 		
 		
-		if(tmp==-1) System.out.println("?");
+		if(n==max) System.out.println("?");
 		else if(max!=0) System.out.println((char)(tmp+65));
 	}
 
+}
 }
