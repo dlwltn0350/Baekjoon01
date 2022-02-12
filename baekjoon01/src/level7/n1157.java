51ed7ccc0f4492b12fdfdb411384a88f1bc3f5c2
dlwltn0350 <dlwltn0350@naver.com>
2022-02-12 23:40:17 +0900

    add 1157

---
+++ baekjoon01/src/level7/n1157.java
@@ -0,0 +1,41 @@
+package level7;
+
+import java.io.*;
+import java.util.Arrays;
+public class n1157 {
+
+	//A~Z: 65 ~ 90, a :97
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s= br.readLine();
+		int max=0, tmp=0;
+		
+		int[] arr=new int[26];
+		Arrays.fill(arr, 0);
+		
+		for(int i=0;i<s.length();i++) {
+			for(int j=65;j<=90;j++) {
+				if((int)s.charAt(i)-j==0) arr[j-65]++;
+				else if((int)s.charAt(i)-j-32==0) arr[j-65]++;
+			}
+		}
+		max=arr[0];
+		for(int i=1;i<26;i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+				tmp=i;
+			}
+			else if(max==arr[i] && max!=0) {
+				tmp=-1;
+				break;
+			}
+		}
+		
+		
+		if(tmp==-1) System.out.println("?");
+		else if(max!=0) System.out.println((char)(tmp+65));
+	}
+
+}
