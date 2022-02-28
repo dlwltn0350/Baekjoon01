2b69a4cc258564c34940b5826cd7056aafb5d215
dlwltn0350 <dlwltn0350@naver.com>
2022-02-28 23:58:13 +0900

    modify 11650 - 람다식 이용

--- baekjoon01/src/level12/n11650.java
+++ baekjoon01/src/level12/n11650.java
@@ -1,11 +1,16 @@
 package level12;
-
+/**
+ * 
+ * 시간복잡도 O(n^2)이상이 되면 안된다.
+ * 내 생각 : 2차원 배열을 사용해야 하므로 O(nlongn)이 걸리는 정렬을 사용하고 이후 y좌표를 비교
+ * 다른 방식 : Arrays.sort()에 람다식을 확장 // Arrays.sort(arr, (arr_1, arr_2) -> {} 의 형식 (?) // Comparator
+ */
 import java.io.*;
 import java.util.Arrays;
 
 public class n11650 {
 
-	public static void insertion_sort(Integer[][] arr, int n) { // x값을 기준으로 정렬
+	public static void insertion_sort(int[][] arr, int n) { // x값을 기준으로 정렬
 		
 		int key=0, key2;
 		
@@ -22,7 +27,7 @@ public class n11650 {
 		}
 	}
 	
-	public static void f(Integer[][] arr, int n) {
+	public static void f(int[][] arr, int n) {
 		int tmp=0;
 		
 		for(int i=1;i<n;i++) {
@@ -32,7 +37,7 @@ public class n11650 {
 		}
 	}
 	
-	public static void swap(int x, int y, Integer[][] arr) {
+	public static void swap(int x, int y, int[][] arr) {
 		int tmp=0;
 		
 		tmp=arr[x][1];
@@ -44,24 +49,38 @@ public class n11650 {
 		arr[y][0]=tmp;
 		
 	}
+	
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
+		
 		String[] str = new String[2];
-		Integer[][] arr = new Integer[n][2];
+		int[][] arr = new int[n][2];
 		
 		for(int i=0;i<n;i++) {
 			str=br.readLine().split(" ");
 			arr[i][0]=Integer.parseInt(str[0]);
 			arr[i][1]=Integer.parseInt(str[1]);
 		}
-		insertion_sort(arr,n);
-		f(arr,n);
+		
+		//insertion_sort(arr,n);
+		//f(arr,n);
+		
+		Arrays.sort(arr,(a,b)->{
+			if(a[0] == b[0]) {
+				return a[1] - b[1];
+			}else {
+				return a[0] - b[0];
+			}
+		});
 	
 		for(int i=0;i<n;i++){
 			System.out.println(arr[i][0]+ " "+ arr[i][1]);
 		}
+		
+		
 	}
 
 }
