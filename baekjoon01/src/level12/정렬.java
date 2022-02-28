95c09956e2c74b9f5accc88e8bb031c746a28618
dlwltn0350 <dlwltn0350@naver.com>
2022-02-28 23:54:53 +0900

    합병

--- baekjoon01/src/level12/정렬.java
+++ baekjoon01/src/level12/정렬.java
@@ -7,6 +7,7 @@ import java.io.*;
 public class 정렬 {
 
 	static int[] arr; 
+	static int[] tmp; //arr배열을 저장할 버퍼
 	
 	public static void selection_sort() {//선택정렬 O(n^2) : n개의 수 중에서 가장 작은 수를 선택해서 앞으로 이동
 		int tmp, index;
@@ -37,7 +38,40 @@ public class 정렬 {
 		
 	}
 	
-	
+	public static void merge_sort() { //합병정렬 -> https://todaycode.tistory.com/54 참고
+		tmp=new int[arr.length];
+		merge_sort(0,arr.length-1);
+	}
+	private static void merge_sort(int start, int end) {
+		int mid;
+		
+		if(start<end) { // 원소가 2개 이상
+			mid=(start+end)/2; // 2개 그룹으로 분할
+			merge_sort(start,mid);
+			merge_sort(mid+1,end);
+			
+			merge(start,mid,end); //병합
+		}
+	}
+	private static void merge(int start, int mid, int end) {
+		tmp=arr.clone(); // arr 배열 복사해둠
+		
+		int part1=start; //왼쪽그룹의 첫번째 인덱스
+		int part2=mid+1; //오른쪽그룹의 첫번째 인덱스
+		int index=start;
+		
+		while(part1<=mid && part2 <=end) {
+			if(tmp[part1]<=tmp[part2]) {
+				arr[index++]=tmp[part1++];
+			}
+			else
+				arr[index++]= tmp[part2++];
+		}
+		
+		for(int i=0; i<=mid-part1; i++) {//왼쪽그룹이 남는경우
+			arr[index+i]=tmp[part1+i];
+		}
+	}
 	
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -49,6 +83,9 @@ public class 정렬 {
 		for(int i=0;i<n;i++)
 			arr[i]=Integer.parseInt(br.readLine());
 		
+		//merge_sort();
+		for(int i=0;i<n;i++)
+			System.out.print(arr[i]+" ");
 		
 	}
 
